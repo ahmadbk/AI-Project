39434a6823b7dd3700136240b201878d564cf95e
ahmadbk <ahmadbk99@gmail.com>
2016-09-08 15:55:48 +0200

    OpenCV Linked

--- AI_Project_Part_2/Main.cpp
+++ AI_Project_Part_2/Main.cpp
@@ -1,2 +1,20 @@
 //Ahmad Bin Khalid
 //Imran Paruk
+
+#include<Windows.h>
+#include <vector>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<fstream>
+#include<iostream>
+#include<String.h>
+#include<ctime>
+#include "opencv2/ml.hpp"
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char * argv[]) {
+}
